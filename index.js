7bb35053cc027fb464cd2516d8829366001a58ac
Nikos Takiris <35358679+nikosTakiris@users.noreply.github.com>
2019-04-08 18:13:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ function redditApi(text, sort, limit) {
   fetch(`https://www.reddit.com/search.json?q=${text}&sort=${sort}&limit=${limit}`)
   .then(response => response.json())
   .then(reddit => reddit.data.children.map(current => {
-     title = current.data.title;
+    
+    title = current.data.title;
     subject = (current.data.selftext !== "") ? current.data.selftext : "";
     thumbnail = (current.data.preview) ? current.data.preview.images[0].source.url : "./images/red.jpg";
     url = current.data.url;
